87f1ba50f273d2ac34460bd71199a41ecbc62419
Soós Andrea <asoos1997@gmail.com>
2023-03-24 10:39:16 +0100

    FirstCommit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddMoviesComponent } from './components/add-movies/add-movies.component';
+import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
+
+const routes: Routes = [
+  { path: '', component: MoviesHomeComponent},
+  { path: 'addMovie', component: AddMoviesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
+import { AddMoviesComponent } from './components/add-movies/add-movies.component';
+import { ButtonComponent } from './components/button/button.component';
+import { MovieTileComponent } from './components/movie-tile/movie-tile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolBarComponent,
+    MoviesHomeComponent,
+    AddMoviesComponent,
+    ButtonComponent,
+    MovieTileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
